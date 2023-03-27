7cced0562fe42ce9fc20edd838f2f8845a6fb4b9
Arturas Aleksandraus <Arturas.Aleksandraus@contextvision.se>
2023-03-27 15:13:40 +0200

    cpu image

--- docker_enter.sh
+++ docker_enter.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-docker build -t party_image --build-arg WORK_DIR=$PWD --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg USER_NAME=$USER environment || exit
 
 # find nvidia gpu
 gpu=$(lspci | tr '[:upper:]' '[:lower:]' | grep -i nvidia)
@@ -8,6 +7,13 @@ printf '==============\n'
 
 # if gpu available add --gpus all
 if [[ $gpu == *' nvidia '* ]]; then
+    docker build -t party_image \
+        --build-arg WORK_DIR=$PWD \
+        --build-arg USER_ID=$(id -u) \
+        --build-arg GROUP_ID=$(id -g) \
+        --build-arg USER_NAME=$USER \
+        -f environment/dockerfile.gpu environment \
+        || exit
     printf 'Nvidia GPU is present:  %s\n' "$gpu"
     docker run -ti --rm \
         -v ~:/home/$USER \
@@ -19,11 +25,19 @@ if [[ $gpu == *' nvidia '* ]]; then
         party_image bash 
 else
     printf 'Nvidia GPU is not present cpu only!\n'
+    docker build -t party_image_gpu \
+        --build-arg WORK_DIR=$PWD \
+        --build-arg USER_ID=$(id -u) \
+        --build-arg GROUP_ID=$(id -g) \
+        --build-arg USER_NAME=$USER \
+        -f environment/dockerfile.gpu environment \
+        || exit
+
     docker run -ti --rm \
         -v ~:/home/$USER \
         -v /mnt:/mnt \
         -v /etc/localtime:/etc/localtime:ro \
         -u $(id -u):$(id -g) \
         --net=host \
-        party_image bash 
+        party_image_gpu bash 
 fi
