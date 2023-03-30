c8b1871902aea1b856200264d835037dd9e00cad
Arturas Aleksandraus <Arturas.Aleksandraus@contextvision.se>
2023-03-30 11:15:50 +0200

    use .venv

--- env.bat
+++ env.bat
@@ -1,17 +1,17 @@
 echo off
 set clean=%1
-if %clean%==clean del environment\venv_win
+if %clean%==clean del .venv
 echo on
 
 @REM create venv if it dose not exists
-if not exist "venv_win" python -m venv environment/venv_win
+if not exist "venv" python -m venv .venv
 
 @REM assume that windows systems has a nvidia GPU
-environment\venv_win\Scripts\pip install -r environment/requirements_gpu.txt
-environment\venv_win\Scripts\pip install -r environment/requirements_base.txt
+.venv\Scripts\pip install -r environment/requirements_gpu.txt
+.venv\Scripts\pip install -r environment/requirements_base.txt
 
 @REM enable sliders in notebooks
-environment\venv_win\Scripts\python -m jupyter nbextension enable --py --sys-prefix widgetsnbextension
+.venv\Scripts\python -m jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
 @REM start env
-environment\venv_win\Scripts\Activate
+.venv\Scripts\Activate
--- env.sh
+++ env.sh
@@ -8,30 +8,32 @@ __banner()
 
 if [[ $1 == "clean" ]]; then 
     __banner Remove old env...
-    rm -rf environment/venv/ 
+    if [[ -d ".venv" ]]; then 
+        rm -rf .venv/ 
+    fi
 fi
 
-python3 -m venv environment/venv
+python3 -m venv .venv
 # find GPU
 gpu=$(lspci | tr '[:upper:]' '[:lower:]' | grep -i nvidia)
 
 # if nvidia is pressent assume that we can install requirements_gpu deps
 if [[ $gpu == *' nvidia '* ]]; then
     __banner 'Nvidia GPU is present:  %s\n instaling deps' "$gpu"
-    ./environment/venv/bin/pip install -r environment/requirements_gpu.txt
+    ./.venv/bin/pip install -r environment/requirements_gpu.txt
 else
     __banner 'Nvidia GPU is not present instaling cpu deps only!'
     # No gpu!? install requirements_cpu deps
-    ./environment/venv/bin/pip install -r environment/requirements_cpu.txt
+    ./.venv/bin/pip install -r environment/requirements_cpu.txt
 fi
 
 __banner Instaling base packages... 
-./environment/venv/bin/pip install -r environment/requirements_base.txt
+./.venv/bin/pip install -r environment/requirements_base.txt
 
 # enable sliders in notebooks
-./environment/venv/bin/python3 -m jupyter nbextension enable --py --sys-prefix widgetsnbextension
+./.venv/bin/python3 -m jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
-chmod +x environment/venv/bin/activate
+chmod +x .venv/bin/activate
 
 # start env
-source ./environment/venv/bin/activate
+source ./.venv/bin/activate
