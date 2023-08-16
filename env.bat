88ba1ca43284497d23e8b6962284f443ac1962cd
Arturas Aleksandraus <Arturas.Aleksandraus@contextvision.se>
2023-08-16 10:52:36 +0200

    remove .

--- env.bat
+++ env.bat
@@ -1,17 +1,17 @@
 echo off
 set clean=%1
-if /I "%clean%" EQU "clean" rmdir /s .venv 
+if /I "%clean%" EQU "clean" rmdir /s venv 
 echo on
 
 @REM create venv if it dose not exists
-if not exist "venv" python -m venv .venv
+if not exist "venv" python -m venv venv
 
 @REM assume that windows systems has a nvidia GPU
-.venv\Scripts\pip install -r environment/requirements_gpu.txt
-.venv\Scripts\pip install -r environment/requirements_base.txt
+venv\Scripts\pip install -r environment/requirements_gpu.txt
+venv\Scripts\pip install -r environment/requirements_base.txt
 
 @REM enable sliders in notebooks
-.venv\Scripts\python -m jupyter nbextension enable --py --sys-prefix widgetsnbextension
+venv\Scripts\python -m jupyter nbextension enable --py --sys-prefix widgetsnbextension
 
 @REM start env
-.venv\Scripts\Activate
+venv\Scripts\Activate
